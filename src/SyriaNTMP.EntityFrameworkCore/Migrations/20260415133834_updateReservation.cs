18490085d88b55c8e828f5bd9266486fe07a5363
Reem <Reem@DESKTOP-OQO8JHP>
2026-04-15 15:40:01 +0200

    add migration

---
+++ src/SyriaNTMP.EntityFrameworkCore/Migrations/20260415133834_updateReservation.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SyriaNTMP.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Reservations",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfRooms",
+                table: "Reservations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfRooms",
+                table: "Reservations");
+        }
+    }
+}
