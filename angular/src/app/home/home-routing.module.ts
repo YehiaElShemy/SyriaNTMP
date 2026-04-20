89afb99ccd105ecb59b09ca7ea3f7944c306e6ee
salah Ragab <s.ragab@aliaict.com>
2026-04-20 11:43:40 +0200

    add landing page

--- angular/src/app/home/home-routing.module.ts
+++ angular/src/app/home/home-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SyriaStatsSystemComponent } from '../pages/syria-NTMP/syria-stats-system.component';
+import { authGuard } from '@abp/ng.core';
+import { HomeComponent } from './home.component';
 
-const routes: Routes = [{ path: '', component: SyriaStatsSystemComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent 
+  },
+  {
+    path: 'syria-stats',
+    component: SyriaStatsSystemComponent,
+    canActivate: [authGuard] 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { AuthService } from '@abp/ng.core';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: false,
@@ -12,7 +13,17 @@ export class HomeComponent {
     return this.authService.isAuthenticated
   }
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {
+    setTimeout(() => {
+      if(this.authService.isAuthenticated){
+        this.router.navigate(['/syria-stats']);
+        console.log('authenticated');
+      }else{
+        console.log('not authenticated');
+      }
+    }, 1000);
+    }
+  
 
   login() {
     this.authService.navigateToLogin();
